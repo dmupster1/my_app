e2469ec7aecf400c01fb8a829d7cff766ad5825b
dmupster1 <usamashaukatt@gmail.com>
2023-03-25 03:35:26 +0500

    hey there

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+String getFullName(String firstName, String lastName) {
+  return '$firstName $lastName';
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(getFullName("Usama", "Shaukat"));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
